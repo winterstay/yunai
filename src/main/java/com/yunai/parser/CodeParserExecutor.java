dbc69d37ae2a880e2248ff36b24ee3574446c29e
winterstay <dengliangfeng520@qq.com>
2026-05-20 16:54:26 +0800

    优化解析器

---
+++ src/main/java/com/yunai/parser/CodeParserExecutor.java
@@ -0,0 +1,34 @@
+package com.yunai.parser;
+
+import com.yunai.yunai.exception.BusinessException;
+import com.yunai.yunai.exception.ErrorCode;
+import com.yunai.yunai.model.enums.CodeGenTypeEnum;
+
+/**
+ * 代码解析执行器
+ * 根据代码生成类型执行相应的解析逻辑
+ *
+ * @author winterstay
+ */
+public class CodeParserExecutor {
+
+    private static final HtmlCodeParser htmlCodeParser = new HtmlCodeParser();
+
+    private static final MultiFileCodeParser multiFileCodeParser = new MultiFileCodeParser();
+
+    /**
+     * 执行代码解析
+     *
+     * @param codeContent 代码内容
+     * @param codeGenType 代码生成类型
+     * @return 解析结果（HtmlCodeResult 或 MultiFileCodeResult）
+     */
+    public static Object executeParser(String codeContent, CodeGenTypeEnum codeGenType) {
+        return switch (codeGenType) {
+            case HTML -> htmlCodeParser.parseCode(codeContent);
+            case MULTI_FILE -> multiFileCodeParser.parseCode(codeContent);
+            default -> throw new BusinessException(ErrorCode.SYSTEM_ERROR, "不支持的代码生成类型: " + codeGenType);
+        };
+    }
+}
+
